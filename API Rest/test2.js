f6f2d2bda327a080027334eda18c6a346f7ecd7a
Félix Arcelin <felix.arcelin@viacesi.fr>
2019-01-22 11:54:14 +0100

    Connection entre la bdd et l'API

--- API Rest/test2.js
+++ API Rest/test2.js
@@ -3,20 +3,31 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+var server = app.listen(3000,"127.0.0.1",function(){
+    var host = server.address().address;
+    var port = server.address().port;
+});
 
-var bdd = mysql.createPool({
+var bdd = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : 'root',
     database : 'web_project'
 });
 
-bdd.getConnection(function(err, connection){
-    if (err) throw err;
-    connection.query('SELECT * from name where idname = 1;',function(error,results,fields){
-        connection.release();
-        if (error) throw error;
-    });
-
+bdd.connect(function(err) {
+    if(err) throw err;
+    console.log('Connected ...')
 })
-
+	
+//rest api to get all customers
+app.get('/name', function (req, res) {
+   bdd.query('select * from name', function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
