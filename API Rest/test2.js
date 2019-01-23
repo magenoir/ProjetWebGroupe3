677159e92b0daab5720d7a7a214775d97f378331
Félix Arcelin <felix.arcelin@viacesi.fr>
2019-01-23 13:55:28 +0100

    Update de l'API REST

--- API Rest/test2.js
+++ API Rest/test2.js
@@ -25,21 +25,44 @@ bdd.connect(function(err) {
     console.log('Connected ...')
 })
 	
-app.get('/name', function (req, res) {
-   bdd.query('select * from name', function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
+app.get('/user', function (req, res) {
+   bdd.query('select * from user', function (error, results, fields) {
+      if (error) return res.status(500).send("There was a problem finding the information to the database.");
+      res.send(JSON.stringify(results));
+      res.end();
 	});
 });
 
- 
-app.post('/add/user/', function(req,res) {
-bdd.query('insert into name set ?',sql,function(error,results,fields){
-        if (error) throw error;
-        res.end(JSON.stringify(results));
+app.get('/user/:id', function (req, res) {
+    bdd.query('select * from user where idname = ?',[req.params.id], function (error, results, fields) {
+       if (error) res.status(500).send("There was a problem finding the information about the user you want");
+       res.send(JSON.stringify(results));
+       res.end();
+
     });
+ });
+ 
+app.post('/user/add', function(req,res) {
+    var sql = {idname: 15, name: 'Yi', firstname:'kes'};
+        bdd.query('insert into user set ?',sql,function(error,results,fields){
+        if (error) throw error;
+        res.status(200).send("Success !")
+        res.end();
+    });    
 })
 
+app.delete('/user/delete/:id', function(req,res){
+    bdd.query('delete from user where idname = ? ',[req.params.id], function(error, results, fields){
+        if (error) res.status(500).send("There was an error deleting the user from database.");
+        res.end();
+    })
+})
 
-
+app.put('/user/put', function(req,res){
+    bdd.query('UPDATE `user` SET `Name`=?, `Firstname`=? where idname=?', [req.body.Name, req.body.Firstname, req.body.idname], function(error, results, fields) { 
+       if(error) throw error;
+       res.send(JSON.stringify(results));
+       
+    })
+})
  
