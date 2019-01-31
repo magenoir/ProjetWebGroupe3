21b60174a10941bd1bff7a75c8685cb3be2c2458
FélixArcelin <33148901+magenoir@users.noreply.github.com>
2019-01-31 15:16:11 +0100

    Update

--- Laravel/blog/resources/views/boutique.blade.php
+++ Laravel/blog/resources/views/boutique.blade.php
@@ -1,9 +1,11 @@
 @include('header3')
 @foreach ($products as $product)
-  <div class="event-container">
-  
-  <div class="event-name">{{ $product->product_name }}</div>
-  </div>  
-    @endforeach
+<div class="evenement">
+    <img class="info" src="images/pull.png" >
+    <p>{{$product->product_name}}</p>
+    <p>{{$product->product_price}} €</p>
+    <img src="images/like.png" style="width: 50px;height: 50px;" alt="like">
+  </div>
+@endforeach
 
-    @include('footer')
+@include('footer')
