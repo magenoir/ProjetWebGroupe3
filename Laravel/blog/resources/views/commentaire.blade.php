c6a20bf60d3e33b2f3854f4720e4419a5c60888c
FélixArcelin <33148901+magenoir@users.noreply.github.com>
2019-01-31 15:17:21 +0100

    Update commentaire.blade.php

--- Laravel/blog/resources/views/commentaire.blade.php
+++ Laravel/blog/resources/views/commentaire.blade.php
@@ -1,18 +1,26 @@
 <div>
+
 	<div class="affcomm">
+	@foreach ($ideventcomment as $idevent) 	
+	
+	@if($event->user_id == idevent->user_id)
 		<div class="commuser">
-			@foreach ($comments as $comment)
-				<p>{{$comment->comment}}</p>
-			@endforeach
+				<p><b>{{$idevent->user_name}} : </b></p>				
 		</div>
 		<div class="comment">
-			
-		</div> 
+		<p>{{$event->comment}}</p>
+		</div>
+	@endif	
+	@endforeach		
 	</div>
+	
+
 	<form action="{{route('Commenter')}}" method="post">
 	@csrf
+	<label for="E-mail"><b>E-Mail :</b></label>
+	<input class="ins" type="text" placeholder=" E-mail" name="E-mailc" required>
 	<textarea class="textcomm" name="Comm" placeholder="Commenter" onkeyup="javascript:capaTextarea(this, 250);"></textarea>
 	<button class="buttoncomm" type="submit">Envoyer</button>
 	</form>
-	
+
 </div>
