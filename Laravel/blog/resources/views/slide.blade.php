81a023c0c497101e62ff72f2b94cb7aef4872c92
FélixArcelin <33148901+magenoir@users.noreply.github.com>
2019-01-31 15:21:31 +0100

    Update slide.blade.php

--- Laravel/blog/resources/views/slide.blade.php
+++ Laravel/blog/resources/views/slide.blade.php
@@ -4,31 +4,31 @@
   <input type="radio" name="image" id="i3">
 
   <div class="slide" id="un">
-    <img src="images/event1.jpg">
+    <img src="images/event1.jpg" alt="image">
 
-    <label for="i3" class="prec"></label>
-    <label for="i2" class="next"></label>
+    <label id="i4" class="prec"></label>
+    <label id="i5" class="next"></label>
 
   </div>
   <div class="slide" id="deux">
-    <img src="images/event2.jpg">
+    <img src="images/event2.jpg" alt="image">
 
-    <label for="i1" class="prec"></label>
-    <label for="i3" class="next"></label>
+    <label id="i6" class="prec"></label>
+    <label id="i7" class="next"></label>
 
   </div>
   <div class="slide" id="trois">
-    <img src="images/event3.jpg"> 
+    <img src="images/event3.jpg" alt="image"> 
 
-    <label for="i2" class="prec"></label>
-    <label for="i1" class="next"></label>
+    <label id="i8" class="prec"></label>
+    <label id="i9" class="next"></label>
 
   </div>
 
   <div class="points">
-    <label class="dots" id="dot1" for="i1"></label>
-    <label class="dots" id="dot2" for="i2"></label>
-    <label class="dots" id="dot3" for="i3"></label>
+    <label class="dots" id="dot1" ></label>
+    <label class="dots" id="dot2" ></label>
+    <label class="dots" id="dot3" ></label>
   </div>
 
 </div>
