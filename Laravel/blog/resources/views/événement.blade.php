12711243200a1b15366b15d1fd05c3622f8ceaf4
FélixArcelin <33148901+magenoir@users.noreply.github.com>
2019-01-31 15:18:41 +0100

    Update événement.blade.php

--- Laravel/blog/resources/views/événement.blade.php
+++ Laravel/blog/resources/views/événement.blade.php
@@ -1,11 +1,46 @@
 
-@include('header')
-
-@foreach ($events as $event)
-  <div class="event-container">
-  <p>{{ $event->event_name }}</p>
-  <div>{{$event->event_description}}</div>
-  </div>  
-    @endforeach
+@include('header_event')
   
-@include('footer')
+  @foreach($events as $event)
+    <div class="evenement">
+      <img class="info" src="images/evenement.jpg" alt="Avatar" >
+       
+      <p><span>Événement : {{$event->event_name}}</span></p>
+      
+      <p>{{$event->event_description}}</p>   
+      
+        <!--<img src="images/like.png" style="width: 50px;height: 50px;"> -->
+        @include('like')
+        <div>
+
+            <div class="affcomm">
+            @foreach ($ideventcomment as $idevent) 	
+              <div class="commuser">
+                  <p><b>{{$idevent->user_name}} : </b></p>				
+              </div>
+                      
+              <div class="comment">
+              <p>{{$idevent->comment}}</p>
+              </div>
+              
+            @endforeach		
+            </div>
+            
+
+            <form action="{{route('Commenter')}}" method="post">
+            @csrf
+            <label id="E-mail"><b>E-Mail :</b></label>
+            <input class="ins" type="text" placeholder=" E-mail" name="E-mailc" required>
+            <textarea class="textcomm" name="Comm" placeholder="Commenter" onkeyup="javascript:capaTextarea(this, 250);"></textarea>
+            <button class="buttoncomm" type="submit">Envoyer</button>
+            </form>
+
+          </div>
+           
+          
+    </div>
+   
+  @endforeach
+     
+  
+@include('footer')  
